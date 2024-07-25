6fc1596040610275bd5e85e237e3430e0a771468
Purusharth Agarwal <141025600+purusharthcodeshere@users.noreply.github.com>
2024-07-25 23:56:26 +0530

    Time: 504 ms (55.81%), Space: 0B (100%) - LeetHub

---
+++ 1179-game-play-analysis-i/1179-game-play-analysis-i.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+SELECT
+    player_id,
+    MIN(event_date) AS first_login
+FROM
+    Activity
+GROUP BY
+    player_id
+ORDER BY
+    player_id ASC,
+    first_login ASC;
