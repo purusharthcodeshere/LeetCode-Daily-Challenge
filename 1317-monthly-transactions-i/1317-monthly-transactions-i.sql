3565f01d4b9e58e82996f95d81921eecda4f98d7
Purusharth Agarwal <purusharthworkshere@gmail.com>
2024-07-31 14:53:55 +0530

    Time: 625 ms (41.24%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -14,3 +14,22 @@ GROUP BY
     country
 ORDER BY
     `month` ASC;
+
+-- Approach
+-- Extract Month and Year:
+-- Use the LEFT function to get the YYYY-MM part from the trans_date. The LEFT() function extracts a number of characters from a string (starting from left).
+
+-- Group By Month and Country:
+-- Group the transactions by the extracted month and country.
+
+-- Count Transactions:
+-- Use COUNT(id) to count all transactions per group.
+
+-- Count Approved Transactions:
+-- Use SUM(state = 'approved') to count approved transactions, leveraging the fact that boolean expressions return 1 for true and 0 for false.
+
+-- Sum Total Amounts:
+-- Use SUM(amount) to sum the transaction amounts for all transactions per group.
+
+-- Sum Approved Amounts:
+-- Use SUM((state = 'approved') * amount) to sum the transaction amounts for approved transactions, ensuring only approved amounts are summed.
