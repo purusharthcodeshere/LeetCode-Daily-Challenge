c3545ab8c5012063225068416075207a68ab3dce
Purusharth Agarwal <purusharthworkshere@gmail.com>
2024-08-04 00:34:34 +0530

    Time: 317 ms (76.6%), Space: 0B (100%) - LeetHub

---
+++ 1452-restaurant-growth/1452-restaurant-growth.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+SELECT
+    DISTINCT visited_on,
+    SUM(amount) OVER w AS amount,
+    ROUND((SUM(amount) OVER w) / 7, 2) AS average_amount
+FROM
+    Customer
+WINDOW w AS (
+    ORDER BY
+        visited_on
+    RANGE 
+        BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW
+)
+LIMIT 6, 999;
