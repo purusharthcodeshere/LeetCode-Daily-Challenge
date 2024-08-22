4a23d2fbf734eee87f549304c33315ca89267cb9
Purusharth Agarwal <purusharthworkshere@gmail.com>
2024-08-22 14:25:20 +0530

    Time: 0 ms (100%), Space: 40 MB (90.99%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -4,6 +4,11 @@ class Solution {
             return 1;
         }
 
+        //Calculate the length of the number in binary representation
+        //Then create a number that has all 1's in the bits place
+        //Of number of bits equal to the length of binary representation of num
+        //Then xor the num and allOnes number
+        
         int bitLength = Integer.toBinaryString(num).length();
         int allOnes = (1 << bitLength) - 1;
         
