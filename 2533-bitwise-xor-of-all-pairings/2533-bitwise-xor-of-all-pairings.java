5c5f2d78cec0159a129c745a5066a73a27b366da
Purusharth Agarwal <purusharthworkshere@gmail.com>
2025-01-16 14:47:38 +0530

    Time: 49 ms (5.17%), Space: 57 MB (94.83%) - LeetHub

---
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        // Get lengths of arrays
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+
+        // Map to store frequency of each number
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        // Add frequencies for nums1 elements
+        // Each element appears n2 times in final result
+        for (int num : nums1) {
+            freq.put(num, freq.getOrDefault(num, 0) + len2);
+        }
+
+        // Add frequencies for nums2 elements
+        // Each element appears n1 times in final result
+        for (int num : nums2) {
+            freq.put(num, freq.getOrDefault(num, 0) + len1);
+        }
+
+        // XOR numbers that appear odd number of times
+        int ans = 0;
+        for (int num : freq.keySet()) {
+            if (freq.get(num) % 2 == 1) {
+                ans ^= num;
+            }
+        }
+
+        return ans;
+    }
+}
